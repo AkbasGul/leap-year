94f1c07a2482e610dca39fa2439e09f9111459c7
Akbas <akkbbss@gmail.com>
2024-01-25 13:03:01 +0100

    okk

--- year.js
+++ year.js
@@ -5,7 +5,7 @@ while (isNaN(year) || year === null) {
 }
 
 if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-  alert((`${year} is a leap year.`));
+  alert(`${year} is a leap year.`);
 } else {
-  alert((`${year} is not a leap year.`));
+  alert(`${year} is not a leap year.`);
 }
