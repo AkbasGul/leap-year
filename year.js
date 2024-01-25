38bfa7a832800de4cd89f1df3caa8650e86b7243
Akbas <akkbbss@gmail.com>
2024-01-25 13:00:03 +0100

    ok

--- year.js
+++ year.js
@@ -1,11 +1,11 @@
 let year = +prompt("Please enter a year");
 
-while (isNaN(year) || (year === null) ) {
+while (isNaN(year) || year === null) {
   year = +prompt("Please enter a valid year!");
 }
 
 if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-  console.log(`${year} is a leap year.`);
+  alert((`${year} is a leap year.`));
 } else {
-  console.log(`${year} is not a leap year.`);
+  alert((`${year} is not a leap year.`));
 }
